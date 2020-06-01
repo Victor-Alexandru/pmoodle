cf0135448258c1cbf5f55860ec2dff1baa04d6b4
Victor-Alexandru <victor21cuciureanu@gmail.com>
2020-06-01 17:46:54 +0300

    register pe backend

--- jwtauth/views.py
+++ jwtauth/views.py
@@ -5,9 +5,12 @@ from django.contrib.auth import get_user_model
 from rest_framework import permissions
 from rest_framework import response, decorators, permissions, status
 from rest_framework_simplejwt.tokens import RefreshToken
+
+from monitor.models import Site_User
 from .serializers import UserCreateSerializer
 
 User = get_user_model()
+
 
 @decorators.api_view(["POST"])
 @decorators.permission_classes([permissions.AllowAny])
@@ -16,6 +19,11 @@ def registration(request):
     if not serializer.is_valid():
         return response.Response(serializer.errors, status.HTTP_400_BAD_REQUEST)
     user = serializer.save()
+    # when the user is ready we need to create a site user
+    site_user = Site_User(user=user, first_name=user.username)
+    site_user.id = user.id
+    site_user.save()
+
     refresh = RefreshToken.for_user(user)
     res = {
         "refresh": str(refresh),
